529cb0f54c198644b63528871c0a7e831ae7b3fa
Christopher Nathan <christophernathan1217@gmail.com>
2021-04-10 00:03:43 -0400

    move sell function to utils

--- utils/trade.py
+++ utils/trade.py
@@ -1,17 +1,19 @@
 import requests, time, json
 from utils import formatting, write_files, account
 
-def buy(api_url, auth, dataframe, long_flag, FEE_PERCENT, cost_basis, CASH_BALANCE):
-    global long_flag, cost_basis, FEE_PERCENT
+def buy(api_url, auth, dataframe, long_flag, cost_basis, FEE_PERCENT, CASH_BALANCE):
     print(dataframe)
     curr_ask = float(dataframe['Ask Price'].iloc[-1])
     max_order_size = min(float(CASH_BALANCE),10000*curr_ask)
     effective_order_size = formatting.truncate(max_order_size/(curr_ask*(1+FEE_PERCENT)),8)
+    print(FEE_PERCENT)
     print(CASH_BALANCE)
     print(min(float(CASH_BALANCE),10000))
     print(curr_ask)
     print(float(dataframe['Ask Price'].iloc[-1]))
     print(min(float(CASH_BALANCE),10000*curr_ask)/curr_ask)
+    print(max_order_size)
+    print(effective_order_size)
     order_details = {
         'type': 'limit',
         'side': 'buy',
@@ -20,6 +22,7 @@ def buy(api_url, auth, dataframe, long_flag, FEE_PERCENT, cost_basis, CASH_BALAN
         'size': effective_order_size # max trade size accounting for fee % and maximum size precision
     }
     order = requests.post(api_url + 'orders', json=order_details, auth=auth)
+    print(order.json())
     order_id = order.json()['id']
     if order.status_code != 200:
         write_files.recordError('trade_errors.csv','BUY',order.status_code,order.json()['message'])
@@ -37,6 +40,40 @@ def buy(api_url, auth, dataframe, long_flag, FEE_PERCENT, cost_basis, CASH_BALAN
         cost_basis = (executed_value+fill_fee)/fill_size
         print('COST BASIS AFTER BUY: ', cost_basis)
         write_files.recordActivity('trade_activity.csv','BUY',fill_price,fill_size,executed_value,fill_fee,cost_basis,0)
+        FEE_PERCENT = account.updateFeePercent('trade_activity.csv')
+    else:
+        delete = requests.delete(api_url + 'orders', auth=auth)
+        write_files.recordError('trade_errors.csv','BUY',order.status_code,'CANCELED')
+
+def sell(api_url, auth, dataframe, long_flag, cost_basis, BTC_BALANCE, FEE_PERCENT):
+    curr_bid = float(dataframe['Bid Price'].iloc[-1])
+    max_order_size = min(float(BTC_BALANCE),10000)
+    effective_order_size = formatting.truncate(max_order_size/(1+FEE_PERCENT),8)
+    order_details = {
+        'type': 'limit',
+        'side': 'sell',
+        'product_id': 'BTC-USD',
+        'price': curr_bid, # order limit is current ask price for fast fill 
+        'size': effective_order_size # max trade size accounting for fee % and maximum size precision
+    }
+    order = requests.post(api_url + 'orders', json=order_details, auth=auth)
+    order_id = order.json()['id']
+    if order.status_code != 200:
+        write_files.recordError('trade_errors.csv','SELL',order.status_code,order.json()['message'])
+    time.sleep(1) # allow time for order to fill
+    order = requests.get(api_url + 'orders/' + order_id, auth=auth)
+    if order.status_code == 200 and order.json()['status'] == 'done':
+        print("SELL SUCCEEDED")
+        text = json.dumps(order.json(), sort_keys=True, indent=4)
+        print (text)
+        long_flag = False
+        executed_value = float(order.json()['executed_value'])
+        fill_size = float(order.json()['size'])
+        fill_price = executed_value/fill_size
+        fill_fee = float(order.json()['fill_fees'])
+        final_cost_basis = ((cost_basis*fill_size)+fill_fee)/fill_size
+        profit = (fill_price-final_cost_basis)*fill_size
+        write_files.recordActivity('trade_activity.csv','SELL',fill_price,fill_size,executed_value,fill_fee,final_cost_basis,profit)
         FEE_PERCENT = account.updateFeePercent('trade_activity.csv')
     else:
         delete = requests.delete(api_url + 'orders', auth=auth)
