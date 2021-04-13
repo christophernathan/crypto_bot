fd783881632764ecc3c6c4008d9ead4e00a93644
Christopher Nathan <christophernathan1217@gmail.com>
2021-04-12 20:17:03 -0400

    unit tests for truncation

--- utils/formatting_test.py
+++ utils/formatting_test.py
@@ -1,4 +1,10 @@
 from utils import formatting
 
-def truncates():
+def test_truncate1():
     assert formatting.truncate(1.001,2) == 1.00
+
+def test_truncate2():
+    assert formatting.truncate(1.2345,4) == 1.2345
+
+def test_truncate3():
+    assert formatting.truncate(1.23456,0) == 1
