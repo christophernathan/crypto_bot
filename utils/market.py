6088f1ebbf387bf8f02a99ca1becd42d364822bc
Christopher Nathan <christophernathan1217@gmail.com>
2021-04-16 22:24:50 -0400

    finished market data unit tests

--- utils/market.py
+++ utils/market.py
@@ -2,13 +2,13 @@ import requests, time
 import pandas as pd
 
 def getMarketData(api_url, auth, BTC_data):
-    curr_data = requests.get(api_url + 'products/BTC-USD/book', auth=auth).json()
-    BTC_data.append([time.time(),curr_data.get('asks')[0][0],curr_data.get('asks')[0][1],curr_data.get('bids')[0][0],curr_data.get('bids')[0][1]])
-    #print(BTC_data)
+    curr_data = requests.get(api_url + 'products/BTC-USD/book', auth=auth)
+    if curr_data.status_code==200:
+        curr_data = curr_data.json()
+        BTC_data.append([time.time(),float(curr_data.get('asks')[0][0]),float(curr_data.get('asks')[0][1]),float(curr_data.get('bids')[0][0]),float(curr_data.get('bids')[0][1])])
     dataframe = pd.DataFrame(BTC_data)
-    #print(dataframe)
     dataframe.columns = ['Unix Timestamp', 'Ask Price', 'Ask Size', 'Bid Price', 'Bid Size']
-    dataframe['Average Price'] = dataframe.apply(lambda row: (float(row['Ask Price'])+float(row['Bid Price']))/2, axis=1)
+    dataframe['Average Price'] = dataframe.apply(lambda row: (row['Ask Price']+row['Bid Price'])/2, axis=1)
     ShortEMA = dataframe['Average Price'].ewm(span=12,adjust=False).mean()
     LongEMA = dataframe['Average Price'].ewm(span=26,adjust=False).mean()
     MACD = ShortEMA - LongEMA
--- utils/market_test.py
+++ utils/market_test.py
@@ -1,16 +1,19 @@
 from utils import market
 from bot import api_url
 import pandas as pd
+import numpy as np
 from pandas._testing import assert_frame_equal
 
-def test_getMarketData(requests_mock):
-    requests_mock.get(api_url+'products/BTC-USD/book',json={"bids":[["63150.71","3008.75745769",1]],"asks":[["63150.73","3009.48614189",1]],"sequence":310102899})
+def test_getMarketData1(requests_mock):
+    requests_mock.get(api_url+'products/BTC-USD/book',json={"bids":[["63150.71","3008.75745769",1]],"asks":[["63150.73","3009.48614189",1]],"sequence":310102899}, status_code=200)
     data = pd.read_json('./utils/mocks/dataframe_mock.json')
     dataframe = pd.DataFrame(data)
-    print()
-    print(market.getMarketData(api_url,{},[]))
-    print(dataframe)
-    print(market.getMarketData(api_url,{},[]).equals(dataframe))
     market_data = market.getMarketData(api_url,{},[])
     del market_data['Unix Timestamp']
     assert_frame_equal(market_data, dataframe, check_dtype=False)
+
+def test_getMarketData2(requests_mock):
+    requests_mock.get(api_url+'products/BTC-USD/book', status_code=401)
+    dataframe = pd.DataFrame([{'Unix Timestamp':0, 'Ask Price':0, 'Ask Size':0, 'Bid Price':0, 'Bid Size':0,'Average Price':0,'ShortEMA':0,'LongEMA':0,'MACD':0,'Signal':0}])
+    market_data = market.getMarketData(api_url,{},[{'Unix Timestamp':0,'Ask Price':0,'Ask Size':0,'Bid Price':0,'Bid Size':0}])
+    assert_frame_equal(market_data,dataframe, check_dtype=False)
