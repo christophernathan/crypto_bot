5de7e1ce1de7d2e2b9474efbb6d1d1b3e11999a5
Christopher Nathan <christophernathan1217@gmail.com>
2021-04-21 20:02:47 -0400

    move mocks and fix unit tests

--- bot/test/account_test.py
+++ bot/test/account_test.py
@@ -2,10 +2,10 @@ from bot.__main__ import api_url
 from bot.utils import account
 
 def test_initializeCostBasis1():
-    assert account.initializeCostBasis('./utils/mocks/trade_activity_mock1.csv') == 0
+    assert account.initializeCostBasis('./bot/mocks/trade_activity_mock1.csv') == 0
     
 def test_initializeCostBasis2():
-    assert account.initializeCostBasis('./utils/mocks/trade_activity_mock2.csv') == 57836.54409126203
+    assert account.initializeCostBasis('./bot/mocks/trade_activity_mock2.csv') == 57836.54409126203
 
 def test_initializeAccountInfo1(requests_mock):
     requests_mock.get(api_url+'accounts',json=[{'currency':'USD','id':'1','available':'2'},{'currency':'BTC','id':'3','available':'4'}])
@@ -24,7 +24,7 @@ def test_updateAccountBalances2(requests_mock):
     assert account.updateAccountBalances(api_url,{}) == (0,0)
 
 def test_updateFeePercent1():
-    assert account.updateFeePercent('./utils/mocks/trade_activity_mock1.csv') == 0.0018
+    assert account.updateFeePercent('./bot/mocks/trade_activity_mock1.csv') == 0.0018
 
 def test_updateFeePercent2():
-    assert account.updateFeePercent('./utils/mocks/trade_activity_mock2.csv') == 0.002
+    assert account.updateFeePercent('./bot/mocks/trade_activity_mock2.csv') == 0.002
--- bot/test/market_test.py
+++ bot/test/market_test.py
@@ -6,7 +6,7 @@ from pandas._testing import assert_frame_equal
 
 def test_getMarketData1(requests_mock):
     requests_mock.get(api_url+'products/BTC-USD/book',json={"bids":[["63150.71","3008.75745769",1]],"asks":[["63150.73","3009.48614189",1]],"sequence":310102899}, status_code=200)
-    data = pd.read_json('./utils/mocks/dataframe_mock.json')
+    data = pd.read_json('./bot/mocks/dataframe_mock.json')
     dataframe = pd.DataFrame(data)
     market_data = market.getMarketData(api_url,{},[])
     del market_data['Unix Timestamp']
